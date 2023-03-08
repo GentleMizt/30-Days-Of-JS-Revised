cc1716ff316405a16247223d9d48ae5e90cd2b50
GentleMizt <olamideiyanda18@gmail.com>
2023-03-08 17:53:07 +0100

    progress

---
+++ Day2/lancer.js
@@ -0,0 +1,18 @@
+    // Starting from Day 2 of '30DAYS OF JAVASCRIPT'
+    let challenge = '30 Days of Javascript'
+    console.log(challenge);
+    console.log(challenge.length);
+    console.log(challenge.toUpperCase());
+    console.log(challenge.toLowerCase());
+    console.log(challenge.substring(0,1));
+    console.log(challenge.substring(3,22));
+    console.log(challenge.includes('String'));
+    console.log(challenge.split(' '));
+    console.log(challenge.replace('Javascript', 'Python'));
+    console.log(challenge.charAt(15));
+    console.log(challenge.lastIndexOf('a'));
+
+
+    let company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+    console.log(company);
+    console.log(company.split(','));
