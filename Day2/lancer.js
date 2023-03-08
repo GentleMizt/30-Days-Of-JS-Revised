7c532fd0a6e434c2ab23462da32239114dd25fcd
GentleMizt <olamideiyanda18@gmail.com>
2023-03-08 19:07:15 +0100

    progress

--- Day2/lancer.js
+++ Day2/lancer.js
@@ -39,4 +39,41 @@
     let pattern2 = /because/gi;
     console.log(quote3.match(pattern2));
 
+    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+    
+    // replacing all the special characters with a space.
+    let cleanedSentence = sentence.replace(/[^a-zA-Z]+/g, ' ').toLowerCase();
+    console.log(cleanedSentence);
+
+    // spliting the cleaned sentence into an array of words.
+    let words = cleanedSentence.split(' ');
+    console.log(words);
+
+    // creating an object to store the count of filtered words.
+    let wordCount ={};
+
+    // looping through the cleaned sentence and assigning words to the word count object and counting it, then assigning a count number to it.
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (!wordCount[word]) {
+            wordCount[word] = 0;
+        }
+        wordCount[word]++;
+    }
+
+    // setting the base value of the highestCount as well as the mostFrequentWord
+    let highestCount = 0;
+    let mostFrequentWord = '';
+
+    // looping through each word in the wordCount object, increasing the count, setting the most occured word
+    for (const word in wordCount){
+        if (wordCount[word] > highestCount) {
+            mostFrequentWord = word;
+            highestCount = wordCount[word];
+        }
+    }
+
+    console.log(`The most frequently occured word in the sentence is ${mostFrequentWord}`);
+
+
 
