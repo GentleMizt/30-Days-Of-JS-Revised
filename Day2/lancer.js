73ea085948ea7d5e769ccf57e22f62507e450c0a
GentleMizt <olamideiyanda18@gmail.com>
2023-03-08 18:13:31 +0100

    progress

--- Day2/lancer.js
+++ Day2/lancer.js
@@ -1,5 +1,5 @@
     // Starting from Day 2 of '30DAYS OF JAVASCRIPT'
-    let challenge = '30 Days of Javascript'
+    let challenge = ' 30 Days of Javascript '
     console.log(challenge);
     console.log(challenge.length);
     console.log(challenge.toUpperCase());
@@ -11,8 +11,14 @@
     console.log(challenge.replace('Javascript', 'Python'));
     console.log(challenge.charAt(15));
     console.log(challenge.lastIndexOf('a'));
+    console.log(challenge.trim());
 
 
     let company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
     console.log(company);
     console.log(company.split(','));
+
+    let sent1 = 'You cannot end a sentence with because because because is a conjunction'
+    console.log(sent1);
+    console.log(sent1.lastIndexOf('because'));
+    console.log(sent1.search('because'));
