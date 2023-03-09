026c68b43c8f17a9ccb0c200031e38c006c6c758
GentleMizt <olamideiyanda18@gmail.com>
2023-03-09 09:36:05 +0100

    progress

--- Day3/lancer.js
+++ Day3/lancer.js
@@ -1,30 +1,42 @@
     // Day 3 of 30 Days of JavaScript
 
-    const now = new Date();
-    console.log(now);
+    // const now = new Date();
+    // console.log(now);
 
-    // Getting Full year
-    console.log(now.getFullYear());
+    // // Getting Full year
+    // console.log(now.getFullYear());
 
-    // Getting Month
-    console.log(now.getMonth());
+    // // Getting Month
+    // console.log(now.getMonth());
 
-    // Getting Date
-    console.log(now.getDate());
+    // // Getting Date
+    // console.log(now.getDate());
 
-    // Getting Day
-    console.log(now.getDay());
+    // // Getting Day
+    // console.log(now.getDay());
 
-    // Getting Hours
-    console.log(now.getHours());
+    // // Getting Hours
+    // console.log(now.getHours());
 
-    // Getting Minutes
-    console.log(now.getMinutes());
+    // // Getting Minutes
+    // console.log(now.getMinutes());
 
-    // Getting Seconds
-    console.log(now.getSeconds());
+    // // Getting Seconds
+    // console.log(now.getSeconds());
 
-    // Getting Time
-    console.log(now.getTime()); // this is the number of seconds passed from January 1, 1970 to March 9, 2023 09:22:41  OR
-    console.log(Date.now()); // Logs same value as above
-    console.log(now.getTime() === Date.now());
+    // // Getting Time
+    // console.log(now.getTime()); // this is the number of seconds passed from January 1, 1970 to March 9, 2023 09:22:41  OR
+    // console.log(Date.now()); // Logs same value as above
+    // console.log(now.getTime() === Date.now());
+
+    // Formatting the values into a readable Format.
+    const now2 = new Date();
+    const year = now2.getFullYear();
+    const month = now2.getMonth() + 1;
+    const date = now2.getDate();
+    const hours = now2.getHours();
+    const minutes = now2.getMinutes();
+
+    console.log(`${date}/${month}/${year}  ${hours}:${minutes}`);
+
+    
