3abe54c9a5345a9207bc3e0c1cba330e025b230e
GentleMizt <olamideiyanda18@gmail.com>
2023-03-10 09:38:32 +0100

    Day 5

---
+++ Day5/lancer5.js
@@ -0,0 +1,8 @@
+    // DAY 5
+    console.log('Day 5 of 30 Days of JavaScript');
+
+    const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+    const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+
+    console.log('Numbers:', numbers);
+    console.log('Fruits:', fruits);
